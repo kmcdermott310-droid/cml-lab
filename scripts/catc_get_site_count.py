f2dbda993adb414c18f10cfb1fdc76333b345b87
kmcdermott310-droid <kmcdermott310@gmail.com>
2026-03-08 11:14:27 -0700

    Enhance site count script with token and API URL
    Added authentication token retrieval and updated API URL.

--- scripts/catc_get_site_count.py
+++ scripts/catc_get_site_count.py
@@ -1,12 +1,21 @@
 import requests
+#1 Import your token function
+from catc_jwt_pull import get_auth_token
 
-url = "/dna/intent/api/v1/site/count"
+#2 Hide SSL warnings
+requests.packages.urllib3.disable_warnings()
 
-payload = None
+#3 Get the token
+token = get_auth_token()
 
-headers = { "Accept": "application/json" }
+#4 Set the Issues API address
+url = "https://sandboxdnac2.cisco.com/dna/intent/api/v1/site/count"
+headers = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
 
+#5 Send the GET request
 response = requests.request('GET', url, headers=headers, data = payload)
 
+#6 Print the results
 print(response.text.encode('utf8'))
+
 
